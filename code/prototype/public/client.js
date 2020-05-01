b164ddcd26171684daf8fd215b0b7fd684d098db
Tom Mendez <tom.mendez-porcel5@etu.univ-lorraine.fr>
2020-05-01 18:31:06 +0200

    feat: avancement du prototype \n Ajout de fonctionnaltiés \n Modification de l'interface

--- code/prototype/public/client.js
+++ code/prototype/public/client.js
@@ -22,17 +22,21 @@ socket.onmessage = function (event) {
   if(num==0){
     num=data.num; //Lors de l'initialisation
     collaborateurs=[num];
+    actualCollaborateurs();
+    actualSet();
     log('Serveur: Bienvenue ' + num)
   }else if(data.numEnvoi!=num&&(data.numDest==num||data.numDest==0)){
-    log('Received: ' + data.message + ' (' + data.numEnvoi + '->' + data.numDest + ')');
+    log('Received: ' + data.message + ' (' + data.numDest + '<-' + data.numEnvoi + ')');
     if(data.message === 'DataRequest'){
       collaborateurs.push(data.numEnvoi);
-      var json = JSON.stringify({ message: 'DataRequestResponse', numEnvoi: num, numDest : data.numEnvoi, users: JSON.stringify(collaborateurs), set: JSON.stringify(set)});
+      var json = JSON.stringify({ message: 'DataUpdate', numEnvoi: num, numDest : data.numEnvoi, users: JSON.stringify(collaborateurs), set: JSON.stringify(set)});
       socket.send(json);
-      log('Sent: DataRequestResponse (' + num + '->' + data.numEnvoi + ')');
-    }else if(data.message === 'DataRequestResponse'){
-      set=data.set;
-      collaborateurs=data.users;
+      log('Sent: DataUpdate (' + num + '->' + data.numEnvoi + ')');
+    }else if(data.message === 'DataUpdate'){
+      set=JSON.parse(data.set);
+      collaborateurs=JSON.parse(data.users);
+      actualCollaborateurs();
+      actualSet();
     }
   }
 }
@@ -42,22 +46,61 @@ socket.onclose = function(event) {
 }
 
 document.querySelector('#close').addEventListener('click', function(event) {
+  collaborateurs.splice(collaborateurs.indexOf(num));
+  actualCollaborateurs();
+  var json = JSON.stringify({ message: 'DataUpdate', numEnvoi: num, numDest : 0, users: JSON.stringify(collaborateurs), set: JSON.stringify(set)});
+  socket.send(json);
+  log('Sent: DataUpdate (' + num + '->' + 0 + ')');
   socket.close();
-  log('Closed connection 😱');
 });
 
 document.querySelector('#broadcast').addEventListener('click', function(event) {
   var json = JSON.stringify({ message: 'Hey there, I am ' + num, numEnvoi: num, numDest: 0 });
   socket.send(json);
-  log('Sent: ' + 'Hey there, I am ' + num);
+  log('Broadcasted: ' + 'Hey there, I am ' + num);
+});
+
+document.querySelector('#submbitChar').addEventListener('click', function(event) {
+  var char = document.querySelector('#char').value;
+  if(char!=''){
+    if(set.includes(char)){
+      log('SmallError: ' + char + ' already in the set');
+    }else{
+      set.push(char);
+      log('Action: ' + char + ' was added to the set');
+      actualSet();
+    }
+  }else{
+    log('SmallError: no char to the set');
+  }
 });
 
 var log = function(text) {
   var li = document.createElement('li');
   li.innerHTML = text;
-  document.getElementById('log').appendChild(li);
+  document.getElementById('log').appendChild(li);var you = "";
 }
 
 window.addEventListener('beforeunload', function() {
   socket.close();
 });
+
+let actualCollaborateurs = function(){
+  $("#collaborateurs").empty();
+  for(let u of collaborateurs) {
+    var you = "";
+    if(u==num){
+      you="(you)";
+    }
+      $(`<li class="collabo">
+            <p>Collaborateur ` + u + ' ' + you + `</p> 
+            <INPUT type="submit" name="ping" value="ping">
+            <INPUT type="submit" name="bloquer" value="bloquer">
+          </li>`).appendTo($("#collaborateurs"));
+  }
+}
+
+let actualSet = function(){
+  $("#set").empty();
+  $(`<p style="text-align: center">Etat acutel du set [` + set + `]</p>`).appendTo($("#set"));
+}
